b697298f0bbf02ad554d027c474c8b172031ada5
zepzincirli <zepicmek@gmail.com>
2025-08-26 21:27:51 +0300

    Add project files.

---
+++ MinimalNoteWebMVC/Controllers/NotesController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MinimalNoteWebMVC.Models;
+using System.Text;
+using System.Text.Json;
+
+namespace MinimalNoteWebMVC.Controllers
+{
+    public class NotesController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        
+        public NotesController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client =_httpClientFactory.CreateClient("NotesApi");
+            var response = await client.GetAsync("notes");
+            if(!response.IsSuccessStatusCode)
+            {
+                return View(new List<Note>());
+            }
+            var notes =await response.Content.ReadFromJsonAsync<List<Note>>();
+            return View(notes);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Note note)
+        {
+            var client = _httpClientFactory.CreateClient("NotesApi");
+            var content = new StringContent(JsonSerializer.Serialize(note),Encoding.UTF8,"application/json");
+            var response= await client.PostAsync("notes",content);
+        
+            if(response.IsSuccessStatusCode)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            ModelState.AddModelError(string.Empty, "Bir hata oluştu.");
+            return View(note);
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            var client = _httpClientFactory.CreateClient("NotesApi");
+            var response = await client.GetAsync($"notes/{id}");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return View(new Note());
+            }
+
+            var note = await response.Content.ReadFromJsonAsync<Note>();
+            return View(note);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var client = _httpClientFactory.CreateClient("NotesApi");
+            var response = await client.DeleteAsync($"notes/{id}");
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var client = _httpClientFactory.CreateClient("NotesApi");
+            var response = await client.GetAsync($"notes/{id}");
+            if (!response.IsSuccessStatusCode)
+            {
+                return NotFound();
+            }
+            var note = await response.Content.ReadFromJsonAsync<Note>();
+            return View(note);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id,Note note)
+        {
+            var client = _httpClientFactory.CreateClient("NotesApi");
+            var content = new StringContent(JsonSerializer.Serialize(note), Encoding.UTF8, "application/json");
+            var response = await client.PutAsync($"notes/{id}",content);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            ModelState.AddModelError(string.Empty, "Bir hata oluştu.");
+            return View(note);
+        }
+    }
+}
---
+++ MinimalNoteWebMVC/Program.cs
@@ -0,0 +1,29 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient("NotesApi",client => 
+{
+    client.BaseAddress = new Uri("https://localhost:7292"); 
+});   
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Notes}/{action=Index}/{id?}");
+
+app.Run();
